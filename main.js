dd5087b7d4a3cd5a51489c8eddf2324cba7a8a1c
ryan <2869888186@qq.com>
2019-08-02 12:09:53 +0800

    周日历完善

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let rl = document.getElementById('calendarBody')
 let table =document.createElement('table')
 rl.appendChild(table)
-let isFold = true
+let isFold = false
 //周日历依赖
 let firstDay, lastDay
 //当前时间
@@ -9,8 +9,187 @@ let currentDate = new Date();
 //当天时间文本
 text.innerText = currentDate.toLocaleDateString();
 
+if (isFold===true){
+
+    //initWeekDate()
+} else{
+    initMonthDate()
+}
+
+//上一周
+pre.onclick = () => {
+    if (isFold===true){
+        setPreCurrentDate();
+        text.innerText = currentDate.toLocaleDateString();
+        initWeekDate()
+    } else{
+
+    }
+
+}
+
+//下一周
+next.onclick = ()=>{
+    if (isFold===true) {
+        setNextCurrentDate();
+        text.innerText = currentDate.toLocaleDateString();
+        initWeekDate()
+    }
+
+}
 
 
+function initMonthDate() {
+    text.innerText = currentDate.toLocaleDateString();
+    table.innerHTML='';
+    baseDate();
+    setMonthDate();
+}
+
+function setMonthDate() {
+    let year = currentDate.getFullYear();
+    let month = currentDate.getMonth();
+    let day = currentDate.getDate();
+
+    let firstDay = new Date(year, month, 1);
+    let firstDayWeek = firstDay.getDay();//获取当月第一天是星期几
+
+    let lastDay = new Date(year, month + 1, 0)
+    let lastDayDate = lastDay.getDate();//获取当月最后一天是什么时候
+
+    let line = Math.ceil((lastDayDate + firstDayWeek) / 7);//获取当月有几个星期
+
+    for (let i = 0; i < line; i++) {
+        let tr = document.createElement('tr')
+        let current, lineIndex;
+        for (let j = 0; j < 7; j++) {
+            let td = document.createElement('td')
+            lineIndex = i * 7 + j;
+            current = lineIndex - firstDayWeek + 1;
+            current = (current <= 0 || current > lastDayDate) ? "" : current;
+            td.innerText = current;
+            if (current === day) {
+                td.className += "current"
+            }
+            tr.appendChild(td)
+        }
+        table.appendChild(tr)
+    }
+}
+
+function initWeekDate(){
+    table.innerHTML='';
+    baseDate();
+    setWeekDate();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//上一周当天
+function setPreCurrentDate() {
+    currentDate.setDate(currentDate.getDate() - 7);
+}
+//下一周当天
+function setNextCurrentDate() {
+    currentDate.setDate(currentDate.getDate() + 7);
+}
+
+
+
+
+function baseDate() {
+    let tr = document.createElement('tr')
+    let head = ['日', '一', '二', '三', '四', '五', '六']
+    for (let i = 0; i < 7; i++) {
+        let th = document.createElement('th')
+        th.innerText = head[i]
+        tr.appendChild(th)
+    }
+    table.appendChild(tr)
+}
+
+function setWeekDate() {
+    let days = getDays(currentDate);
+
+    let tr = document.createElement('tr')
+    for (let i = 0; i < 7; i++) {
+        let td = document.createElement('td')
+        td.innerText = days[i].getDate();
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+
+    //重新赋值
+    lastDay = getDays(currentDate)[6]; //本周的最后一天
+    firstDay = getDays(currentDate)[0]; //本周的第一天
+}
+
+//在表格中显示一周的日期
+function getDays(date) {
+    let days = new Array();
+    for (let i = 1; i <= 7; i++) {
+        days[i - 1] = getInWeek(date,i);
+    }
+    return days;
+}
+
+//取得当前日期一周内的某一天
+function getInWeek(date,i) {
+    let n = date.getDay(); //今天星期几
+    let start = new Date();
+    start.setDate(date.getDate() - n + i -1); //一周内的第i天
+    return start;
+}
 
 //
 // let year = currentDate.getFullYear();
